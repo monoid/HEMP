f8d4e949c2520d3b198b52b15d072c63317aed53
Ivan Boldyrev <lispnik@gmail.com>
2011-06-11 22:56:19 +0700

    Change formatting and and comments to HempTypes.

--- hemp-types.hs
+++ hemp-types.hs
@@ -2,6 +2,8 @@ module HempTypes where
 
 import HempDecl
 
+-- Base type of primitive types, arrays and stream.
+-- For example, base type of array (or stream) of integer is integer.
 baseArithType :: Type -> Maybe PrimitiveType
 baseArithType (PrimitiveType a) = Just a
 baseArithType (ArrayType n a) = baseArithType a
@@ -12,42 +14,45 @@ numeric a = case a of
                  NumericType _ -> True
                  otherwise -> False
 
+-- Common primitive type
 commonType :: PrimitiveType -> PrimitiveType -> Maybe PrimitiveType
-commonType a b = if a > b then
-                    commonType b a
-                 else if a == b then
-                      Just a
-                 else case (a, b) of
-                           (NumericType a', NumericType b') -> Just (NumericType (commonNumType a' b'))
-                           _ -> Nothing
+commonType a b
+           | a > b = commonType b a
+           | a == b = Just a
+commonType (NumericType a) (NumericType b) = Just (NumericType (commonNumType a b))
+commonType _ _ = Nothing
 
+
+-- Common numeric type
 commonNumType :: NumericType -> NumericType -> NumericType
-commonNumType a b = if a > b then
-                       commonNumType b a
-                    else case (a, b) of
-                         (RealTypes (FractionalType a'), ComplexType b') -> ComplexType (max a' b')
-                         (a', b') -> max a' b'
+commonNumType a b
+              | a > b = commonNumType b a
+commonNumType (RealTypes (FractionalType a)) (ComplexType b) = ComplexType (max a b)
+commonNumType a b = max a b
 
+
+-- Common types for arrays and primitive types
 congruentType :: Type -> Type -> Maybe Type
-congruentType a b = case (a, b) of
-                         -- TODO: get rid of m and n at all...  Do nested
-                         -- array instead. Or substract 1 from both m and n
-                         (ArrayType m a', ArrayType n b') ->
-                               do
-                                   c <- congruentType a' b'
-                                   return (ArrayType 1 c) -- 1 is just a stub
-                         (PrimitiveType a', PrimitiveType b') ->
-                               do
-                                   c <- commonType a' b'
-                                   return (PrimitiveType c)
-                         (PrimitiveType a', ArrayType m b') ->
-                               do
-                                   c <- congruentType (PrimitiveType a') b'
-                                   return (ArrayType 1 c) -- 1 is just a stub
-                         (ArrayType m b', PrimitiveType a') ->
-                               do
-                                   c <- congruentType (PrimitiveType a') b'
-                                   return (ArrayType 1 c) -- 1 is just a stub
+congruentType a b =
+    case (a, b) of
+    -- TODO: get rid of m and n at all...  Do nested
+    -- array instead. Or substract 1 from both m and n
+         (ArrayType m a', ArrayType n b') ->
+                    do
+                        c <- congruentType a' b'
+                        return (ArrayType 1 c) -- 1 is just a stub
+         (PrimitiveType a', PrimitiveType b') ->
+                    do
+                        c <- commonType a' b'
+                        return (PrimitiveType c)
+         (PrimitiveType a', ArrayType m b') ->
+                    do
+                        c <- congruentType (PrimitiveType a') b'
+                        return (ArrayType 1 c) -- 1 is just a stub
+         (ArrayType m b', PrimitiveType a') ->
+                    do
+                        c <- congruentType (PrimitiveType a') b'
+                        return (ArrayType 1 c) -- 1 is just a stub
                                
-                         _ -> Nothing
+         _ -> Nothing
 
